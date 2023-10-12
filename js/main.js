d2c99025ea63ba5c03faaf2cbb4a1963a76266d5
adereb-web <ireoluwaadegoke7@gmail.com>
2023-10-12 13:25:33 +0100

    glow

--- js/main.js
+++ js/main.js
@@ -72,3 +72,32 @@
         dots: true,
         items: 1
     });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
